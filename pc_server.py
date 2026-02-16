81cd47ef9f100faf509e48f99864c3edbd4d6535
Deb <giornoexe0@gmail.com>
2026-02-16 14:39:20 +0530

    Reduced redundant logic and LoC with slight tweaks, can also connect to the host now. Will work on the interface henceforth

--- pc_server.py
+++ pc_server.py
@@ -12,11 +12,11 @@ def accept_connection():
     while True:
         data, ret_address = sock.recvfrom(1024)
         data = data.decode('utf-8')
-
+        print(data)
         if data == 'controller':
             sock.sendto(f"{socket.gethostname()}".encode('utf-8'), ret_address)
         
 
-connection_thread = threading.Thread(target=accept_connection(), args=())
+connection_thread = threading.Thread(target=accept_connection, args=())
 connection_thread.start()
     
